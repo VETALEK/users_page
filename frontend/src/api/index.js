65e74bb97d67c81c3651890982bd1b6d4d453808
Vitalik Avdyeyenko <dalshe.bude.sobaka@gmail.com>
2023-01-07 22:36:57 +0200

    feat(requests): describe requests for users

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,10 +1,38 @@
 import { apiBaseUrl } from '../config';
 
-export const request = (endpoint, method) => {
-  return fetch(`${apiBaseUrl}${endpoint}`, { method })
-    .then((response) => {
+export const request = (endpoint, method, data) => {
+  const requestInit = {
+    method,
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  };
+
+  if (data) {
+    Object.assign(
+      requestInit,
+      { 
+        body: JSON.stringify(data),
+      },
+    );
+  }
+
+  return fetch(
+    `${apiBaseUrl}${endpoint}`,
+    requestInit,
+  ).then((response) => {
       return response.json();
-    })
+    });
 };
 
-export const getUsers = () => request('/users', 'GET');
+export const getAllUsers = () => request('/users', 'GET');
+
+export const getUserById = (id) => request(`/users/${id}`, 'GET');
+
+export const createUser = (user) => request('/users', 'POST', user);
+
+export const updateUserById = (id, user) => request(`/users/${id}`, 'PATCH', user);
+
+export const createOrUpdateUser = (user) => request('/users', 'PUT', user);
+
+export const deleteUserById = (id) => request(`/users/${id}`, 'DELETE');
